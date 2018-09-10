b9b613031cf200fd2130596efe284bc14767f9ee
DESKTOP-M4H4KP2\Nerval Gravie <nervalgravie@gmail.com>
2018-09-10 17:35:37 +0800

    add TankTrack Class

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is use to set maximum driving force, and to apply for forces to the tank
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))//, hidecategories = ("Collision"))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	
+};
