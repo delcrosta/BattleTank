12c8f2c7202abb0e45c8312fa15c8b74e0fb9177
DESKTOP-M4H4KP2\Nerval Gravie <nervalgravie@gmail.com>
2018-09-12 11:17:47 +0800

    Modify TankMovementComponent Architecture

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,12 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
 
 
+void UTankMovementComponent::initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+		RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("intend move forward throw : %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO prevent double-speed doe to dual control use
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -5,17 +5,25 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
- * 
+ * responsible from driving the tank track 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
 
 public:
+		UFUNCTION(BlueprintCallable, Category = Setup)
+		void initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
 		UFUNCTION(BlueprintCallable, Category = Input)
 		void IntendMoveForward(float Throw);
-	
+
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 	
 };
