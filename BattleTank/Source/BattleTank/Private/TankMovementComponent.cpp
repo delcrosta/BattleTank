63f1103ed6fbec02c80d9f7862711855b44adf2a
DESKTOP-M4H4KP2\Nerval Gravie <nervalgravie@gmail.com>
2018-09-12 11:44:03 +0800

    Fixing TankMovementComponent issues

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -8,16 +8,24 @@
 
 void UTankMovementComponent::initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+
 	LeftTrack = LeftTrackToSet;
-		RightTrack = RightTrackToSet;
+	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("intend move forward throw : %f"), Throw);
-
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
+	// TODO prevent double-speed doe to dual control use
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	UE_LOG(LogTemp, Warning, TEXT("Intend turn right"));
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 	// TODO prevent double-speed doe to dual control use
 }
